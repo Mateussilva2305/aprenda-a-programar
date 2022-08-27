0a020a8c2bbb9bd5b6ed1475b0173c4ee98837bd
mateus silva <mateush.nsilva@gmail.com>
2022-08-27 13:57:36 -0300

    js

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome = 'mateus'
+let email = 'mateush.nsilva@gmail.com'
+let profissao = 'estudante'
+let idade = 17
+let lugar = 'poços de caudas'
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
